13600488dbd498b7f3e0f6b5eefb715ff6070bf9
Lola Wenzel <loretawenzel@usp.br>
2023-04-03 23:24:07 -0300

    update

--- data/generate_data.py
+++ data/generate_data.py
@@ -1,18 +1,22 @@
 import cv2
 
-size = 256
-file = 1600
 
-img = cv2.imread(f"image/dbe/DBE_4_900_cut_2.tif")
+def cut_images(file, save, size=224):
+
+    img = cv2.imread(file)
 
 
-x_axis = (img.shape[0] // size) * size
-y_axis = (img.shape[1] // size) * size
+    x_axis = (img.shape[0] // size) * size
+    y_axis = (img.shape[1] // size) * size
 
-i = 0
-for x in range(0, x_axis, size):
-    for y in range(0, y_axis, size):
-        cv2.imwrite(f"image/dbe/classify_{file}_size{size}/{i}.png", img[x : x + size, y : y + size])
-        i += 1
+    i = 0
+    for x in range(0, x_axis, size):
+        for y in range(0, y_axis, size):
+            cv2.imwrite(f"{save}/{i}.png", img[x : x + size, y : y + size])
+            i += 1
 
+images_name = ["DBE3", "DBE4", "DBE4a"]
+
+for file in images_name:
+    cut_images(f"images/{file}.tif", f"images/{file}_cut")
 
